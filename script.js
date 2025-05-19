758e0ddd8022e94448d770b0598def83e0ba5200
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-19 11:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListner("DOMContentLoaded", () => {
+	console.log("DOM load success");
+	body.innetHTML = `
+	<p>
+		DOM load success
+	</p>
+	`;
+})
